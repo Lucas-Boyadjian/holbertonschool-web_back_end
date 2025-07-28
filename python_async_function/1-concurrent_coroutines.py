1b4e3e38f8986bb8dad99f39c1a7e1e65ae54949
Lucas-Boyadjian <10462@holbertonstudents.com>
2025-07-28 16:21:17 +0200

    Implement wait_n coroutine and measure_time function; add main files for testing

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python3
-import wait_random from 0-async_generator
+
+import importlib
+from typing import List
+import asyncio
+
+wait_random = importlib.import_module('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Launch wait_random n times with max_delay and return delays in order of completion.
+    """
+    listDelays = []
+    tasks = []
+    for i in range(n):
+        tasks.append(asyncio.create_task(wait_random(max_delay)))
+    for task in asyncio.as_completed(tasks):
+        listDelays.append(await task)
+    return listDelays
---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import importlib
+import asyncio
+import time
+
+wait_n = importlib.import_module('1-concurrent_coroutines').wait_n
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measure the average runtime of wait_n(n, max_delay).
+    """
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.time()
+    total_time = end - start
+    return total_time / n
+   
