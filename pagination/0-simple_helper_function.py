a8c1820c29748b9d48c6949e02dcb419fcac7376
Lucas-Boyadjian <10462@holbertonstudents.com>
2025-08-11 11:09:39 +0200

    Refactor index_range function type hint to use Tuple instead of Tuple[int, int]

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -7,7 +7,7 @@ start and end indexes for pagination.
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple:
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -11,7 +11,7 @@ from typing import List, Tuple
 This module provides a helper function for calculating
 start and end indexes for pagination.
 """
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple:
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
