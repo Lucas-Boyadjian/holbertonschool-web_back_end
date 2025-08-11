8b14af6fd92c49c3d1f06d37ebfd0b202e42422d
Lucas-Boyadjian <10462@holbertonstudents.com>
2025-08-11 11:10:18 +0200

    Change return type hint of index_range function from Tuple to tuple

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -4,10 +4,8 @@ This module provides a helper function for calculating
 start and end indexes for pagination.
 """
 
-from typing import Tuple
 
-
-def index_range(page: int, page_size: int) -> Tuple:
+def index_range(page: int, page_size: int) -> tuple:
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
