a8afcec4386b2e4d3c930db2e186c3e6a32e3201
Lucas-Boyadjian <10462@holbertonstudents.com>
2025-08-19 09:53:05 +0200

    Refactor getFullBudgetObject to define fullBudget variable for improved readability

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -2,13 +2,15 @@ import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
-  return {
+  const fullBudget = {
     ...budget,
     getIncomeInDollars() {
       return `$${this.income}`;
     },
     getIncomeInEuros() {
       return `${this.income} euros`;
-    }
+    },
   };
+
+  return fullBudget;
 }
