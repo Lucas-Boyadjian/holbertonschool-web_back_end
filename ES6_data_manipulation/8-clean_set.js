45f8ecb93a8f9af5e5bb40852109e98699f9b3a1
Lucas-Boyadjian <10462@holbertonstudents.com>
2025-08-26 14:21:49 +0200

    Refactor cleanSet function to improve startString validation; combine checks for empty string and type

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,7 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') return '';
-
+  if (startString === '' || typeof startString !== 'string') {
+    return '';
+  }
   const strArray = [];
   for (const element of set) {
     if (element.startsWith(startString)) strArray.push(element.slice(startString.length));
