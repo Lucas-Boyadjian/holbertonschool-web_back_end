60e3f5f1f61a6cb6609e35e9672b177a0282a378
Lucas-Boyadjian <10462@holbertonstudents.com>
2025-08-27 10:25:42 +0200

    Refactor handleProfileSignup for clarity in promise result mapping; simplify loadBalancer by removing unnecessary then

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,8 +5,17 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) => results.map((result) => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
-  })));
+  ]).then((results) => results.map((result) => {
+    if (result.status === 'fulfilled') {
+      return {
+        status: result.status,
+        value: result.value
+      };
+    } else {
+      return {
+        status: result.status,
+        value: result.reason.toString()
+      };
+    }
+  }));
 }
