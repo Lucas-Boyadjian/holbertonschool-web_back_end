936f49a810704acac6d26aace2156f4cb55abae2
Lucas-Boyadjian <10462@holbertonstudents.com>
2025-09-03 10:33:00 +0200

    Refactor countStudents and HTTP server to return formatted student list and improve error handling

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -8,13 +8,25 @@ const app = http.createServer((req, res) => {
     return;
   }
   if (req.url === '/students') {
-    const dataBase = process.argv[2];
-    countStudents(dataBase)
-      .then((data) => {
-        res.end(`This is the list of our students\n${data}`);
+    const databasePath = process.argv[2];
+    const originalLog = console.log;
+    let output = '';
+    console.log = (msg) => { output += `${msg}\n`; };
+
+    res.write('This is the list of our students\n');
+
+    countStudents(databasePath)
+      .then(() => {
+        console.log = originalLog;
+        res.end(output.trim());
+      })
+      .catch(() => {
+        console.log = originalLog;
+        res.end('Cannot load the database');
       });
   }
 });
+
 app.listen(1245);
 
 module.exports = app;
